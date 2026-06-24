4d673c871defec13198698186d5e3e1e08eaffbc
llGiovannall <laqi775@gmail.com>
2026-06-24 19:26:33 -0300

    feat: create redacaoPage to fetch python API

---
+++ claude-assistant/app/redacao/redacaoPage.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import React, { useState } from "react";
+
+export default function redacaoPage() {
+const [redacao, setRedacao] = useState("");
+ const [resultado, setResultado] = useState<any>(null);
+
+    const corrigir = async () => {
+  const response = await fetch("http://127.0.0.1:8000/corrigir", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      texto: redacao
+    }),
+  });
+
+  const dados = await response.json();
+
+ return (
+    <div>
+      <textarea
+        value={redacao}
+        onChange={(e) => setRedacao(e.target.value)}
+      />
+
+      <button onClick={corrigir}>
+        Corrigir
+      </button>
+
+      {resultado && (
+        <div>
+          <p>Competência 1: {resultado.competencia_1}</p>
+          <p>Competência 2: {resultado.competencia_2}</p>
+          <p>Competência 3: {resultado.competencia_3}</p>
+          <p>Competência 4: {resultado.competencia_4}</p>
+          <p>Competência 5: {resultado.competencia_5}</p>
+          <p>Nota Total: {resultado.nota_total}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+}
