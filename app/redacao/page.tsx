e0ac50d735fadfdf30563c25612ae50c562d2a8e
llGiovannall <laqi775@gmail.com>
2026-06-25 15:02:22 -0300

    feat: corrigir button working with resultado and more max length

--- app/redacao/page.tsx
+++ app/redacao/page.tsx
@@ -24,7 +24,7 @@ export default function RedacaoPage() {
   return (
 
     <div className="mt-10 p-10">
-    <form className="flex flex-col mx-auto gap-2 max-w-lg">
+    <form className="flex flex-col mx-auto gap-2 max-w-lg" onSubmit={(e) => e.preventDefault()}>
         <fieldset className="contents">
             <div className="flex flex-col">
                 <label htmlFor="input" className="font-semibold text-lg">
@@ -34,35 +34,36 @@ export default function RedacaoPage() {
   name="input" 
   id="input" 
   rows={5} 
-  maxLength={256} 
+  maxLength={3000} 
   required
   value={redacao}
   onChange={(e) => setRedacao(e.target.value)}
   className="rounded-lg p-4 bg-black/5 border-2 border-solid border-black/10 font-mono font-medium text-sm"
 />
             </div>
-            <button type="submit" onClick = {corrigir}
+            <button type="button" onClick = {corrigir }
 				className="rounded-lg p-3 bg-green-500/20 border-2 border-solid border-green-500/20 transition-colors hover:bg-green-500/40 font-medium text-base leading-none flex flex-row items-center justify-center gap-2"><svg
 					xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="18" height="18">
-					<path fill-rule="evenodd"
+					<path fillRule="evenodd"
 						d="M14.615 1.595a.75.75 0 01.359.852L12.982 9.75h7.268a.75.75 0 01.548 1.262l-10.5 11.25a.75.75 0 01-1.272-.71l1.992-7.302H3.75a.75.75 0 01-.548-1.262l10.5-11.25a.75.75 0 01.913-.143z"
-						fillRule="evenodd"
-clipRule="evenodd"></path>
+						
+clipRule="evenodd"/>
 				</svg>
 				<span className="font-bold">Corrigir</span>
 			</button>
-        </fieldset>
+   </fieldset>
     </form>
-    {resultado && (
-        <div>
-          <p>Competência 1: {resultado.competencia_1}</p>
-          <p>Competência 2: {resultado.competencia_2}</p>
-          <p>Competência 3: {resultado.competencia_3}</p>
-          <p>Competência 4: {resultado.competencia_4}</p>
-          <p>Competência 5: {resultado.competencia_5}</p>
-          <p>Nota Total: {resultado.nota_total}</p>
-        </div>
-      )}
+          {resultado && (
+  <div  className="mx-auto max-w-lg mt-6">
+    <p>Competência 1: {resultado.competencia_1}</p>
+    <p>Competência 2: {resultado.competencia_2}</p>
+    <p>Competência 3: {resultado.competencia_3}</p>
+    <p>Competência 4: {resultado.competencia_4}</p>
+    <p>Competência 5: {resultado.competencia_5}</p>
+    <p>Nota Total: {resultado.nota_total}</p>
+  </div>
+)}
+     
     </div>
 
     
