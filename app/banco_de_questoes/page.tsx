a93496087a79f4d39fda98f7c26946cd5519f300
llGiovannall <laqi775@gmail.com>
2026-07-12 21:46:33 -0300

    feat: creating base front of banco de questoes

---
+++ app/banco_de_questoes/page.tsx
@@ -0,0 +1,10 @@
+ 
+ export default function bancoDeQuestoes(){
+    return (
+ 
+ <div>
+      <h1 className="text-4xl font-bold text-white"> Brainy! 
+        Seu assistente de IA para conquistar sua aprovação. </h1>
+      <p className="text-zinc-400">Pratique redação, revise conteúdos e chegue no dia do vestibular preparado.</p>
+    </div>)
+ }
