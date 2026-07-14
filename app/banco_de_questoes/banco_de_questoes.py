13aee444b8a86cd0bd1819d906fc6ca0728f0cbe
llGiovannall <laqi775@gmail.com>
2026-07-13 22:51:02 -0300

    feat: python reading and extracting questions from pdf

--- app/banco_de_questoes/banco_de_questoes.py
+++ app/banco_de_questoes/banco_de_questoes.py
@@ -1,8 +1,10 @@
-import json
-import postgresql
 
-with open("questoes.json", "r", encoding="utf-8") as f:
-    questoes = json.load(f)
+import pdfplumber
 
-    conn = postgresql.connect("bancoquestoes")
-cursor = conn.cursor()
+with pdfplumber.open("app/banco_de_questoes/ENEM_2019_P1_CAD_01_DIA_1_AZUL.pdf") as pdf:
+    for pagina in pdf.pages:
+        texto = pagina.extract_text()
+        print(texto)
+
+  
+
