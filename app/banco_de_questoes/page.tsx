859ddd80c8f06355b12f866d5c89c15215f1f783
llGiovannall <laqi775@gmail.com>
2026-07-28 20:03:35 -0300

    feat: new page for questions bank

--- app/banco_de_questoes/page.tsx
+++ app/banco_de_questoes/page.tsx
@@ -1,10 +1,20 @@
+ "use client";
  
+ 
+ import { useRouter } from "next/navigation";
  export default function bancoDeQuestoes(){
+  const router = useRouter();
     return (
  
- <div>
-      <h1 className="text-4xl font-bold text-white"> Brainy! 
-        Seu assistente de IA para conquistar sua aprovação. </h1>
-      <p className="text-zinc-400">Pratique redação, revise conteúdos e chegue no dia do vestibular preparado.</p>
-    </div>)
+  <div className="flex flex-col gap-3 md:flex-row md:items-center md:justify-between">
+         <div className="max-w-xl md:w-3/5">
+           <div onClick={()=> router.push("banco_de_questoes/materias/ingles")}className="mt-2 flex w-64 h-32 items-center gap-4 rounded-2xl bg-white/10 px-3 py-5 text-2x1"  >
+             <div>
+               <div className="font-large" >Inglês</div>
+ 
+             </div>
+           </div>
+         </div>
+         </div>
+         )
  }
