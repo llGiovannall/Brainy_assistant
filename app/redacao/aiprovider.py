1c0945cf6a0b9b8352fdf5497363f85278db4abd
llGiovannall <laqi775@gmail.com>
2026-07-28 20:09:03 -0300

    feat: trade groq ai api for gemini api and change prompt to evaluate essay sections

--- app/redacao/aiprovider.py
+++ app/redacao/aiprovider.py
@@ -1,19 +1,19 @@
-from groq import Groq # type: ignore
+from google import genai # type: ignore
 from dotenv import load_dotenv
 import json
 import os
 from pathlib import Path
 
 
-env_path = Path(__file__).parent.parent / ".env.local"
+env_path = Path(__file__).parent.parent.parent / ".env.local"
 
 print("ENV PATH:", env_path)
 
 load_dotenv(env_path)
-print("API KEY =", os.getenv("GROQ_API_KEY"))
-client = Groq(
-    api_key=os.getenv("GROQ_API_KEY")
-)
+print("API KEY =", os.getenv("GEMINI_API_KEY"))
+client = genai.Client(
+    api_key=os.getenv("GEMINI_API_KEY"))
+
 
 print("cliente criado")
 
@@ -51,7 +51,24 @@ Competência 5:
 Elaboração de proposta de intervenção para o problema abordado.
 
 A nota total deve ser a soma das cinco competências.
+Além das competências, avalie cada seção da redação.
 
+Descrição das seções:
+Introdução: É o primeiro parágrafo da redação, ele tem o dever de introduzir o tema da redação citando dois fatores como obstáculos 
+para serem argumentados no desenvolvimento 1 e 2 e resolvidos na conclusão.
+Desenvolvimento 1: É o segundo parágrafo da redação
+Desenvolvimento 2: É o terceiro parágrafo da redação
+Conclusão: É o último parágrafo da redação
+
+Para cada uma delas atribua uma nota de 0 a 100 considerando:
+
+- Clareza
+- Organização
+- Argumentação
+- Coesão
+- Adequação ao gênero
+
+Se uma seção não existir, retorne nota 0.
 Retorne APENAS um JSON válido.
 
 
@@ -63,7 +80,11 @@ Formato:
     "competencia_3": 0,
     "competencia_4": 0,
     "competencia_5": 0,
-    "nota_total": 0
+    "nota_total": 0,
+    "introducao": 0,
+    "desenvolvimento_1": 0,
+    "desenvolvimento_2": 0,
+    "conclusao": 0,
 }}
 
 Redação:
@@ -71,19 +92,29 @@ Redação:
 {redacao}
 """
 
-    response = client.chat.completions.create(
-        model="llama-3.3-70b-versatile",
-        messages=[{"role": "user", "content": prompt}]
+    response = client.models.generate_content(
+         model="gemini-2.0-flash",
+       contents=prompt
     )
 
-    resultado = response.choices[0].message.content
+    resultado = response.text
     resultado = resultado.replace("```json", "").replace("```", "").strip()
 
-    dados = json.loads(resultado)
+    try:
+        dados = json.loads(resultado)
+     
+    except json.JSONDecodeError:
+        print(resultado)
+        raise
+
     return dados
+                          
 
 
-if __name__ == "__main__": #jeito python de dizer "roda isso quando executar o arquivo diretamente".
+
+
+if __name__ == "__main__":
     redacao = input("Cole a redação aqui: ")
     dados = corrigir_redacao(redacao)
     print(f"Nota total: {dados.get('nota_total')}")
+
