40e43528d08ab55621edfb97f92cdbea5b913710
llGiovannall <laqi775@gmail.com>
2026-07-28 20:01:47 -0300

    feat: new inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,17 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+
+
+import { Inter } from "next/font/google";
+
+const inter = Inter({ 
+  subsets: ["latin"], 
+  variable: "--font-sans"
+ });
+
+
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -24,10 +35,11 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      lang="pt-BR"
+      className={`${geistSans.variable} ${geistMono.variable}${inter.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+       <body className="font-sans">{children}</body>
     </html>
+    
   );
 }
