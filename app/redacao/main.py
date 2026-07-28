1e3740f7c33b71bbb2703f59d577965e29b15d9c
llGiovannall <laqi775@gmail.com>
2026-07-28 20:10:37 -0300

    feat: new python command to save essay evaluation (under construction) and putting sections asked in the prompt inside resultado

--- app/redacao/main.py
+++ app/redacao/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
-from aiprovider import corrigir_redacao
+from .aiprovider import corrigir_redacao
 from fastapi.middleware.cors import CORSMiddleware
+from pathlib import Path
 
 app = FastAPI()
 app.add_middleware(
@@ -11,14 +12,22 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+result_path = Path(__file__).parent.parent 
+CAMINHO_SAIDA = result_path / "saida.txt"
+
 class RedacaoRequest(BaseModel):
     texto: str
 
+def salvar_resultado(resultado):
+   with open("CAMINHO_SAIDA", "w", encoding="utf-8") as arquivo:
+        arquivo.write(str(resultado))
+print("arquivo salvo")
 
 @app.post("/corrigir")
 def corrigir(request: RedacaoRequest):
     print(request)
     resultado = corrigir_redacao(request.texto)
+    salvar_resultado(resultado)
 
     return {
         "competencia_1": resultado["competencia_1"],
@@ -27,8 +36,15 @@ def corrigir(request: RedacaoRequest):
         "competencia_4": resultado["competencia_4"],
         "competencia_5": resultado["competencia_5"],
         "nota_total": resultado["nota_total"],
+        "introducao": resultado["introducao"],
+        "desenvolvimento_1": resultado["desenvolvimento_1"],
+        "desenvolvimento_2": resultado["desenvolvimento_2"],
+        "conclusao": resultado["conclusao"],
+    
     }
 
+
+        
 @app.get("/")
 def root():
     return {"mensagem": "API funcionando"}
