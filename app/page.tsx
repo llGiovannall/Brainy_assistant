1a0d506a387120dd5a5864a1fe56cbe0672a9414
llGiovannall <laqi775@gmail.com>
2026-07-28 20:06:33 -0300

    fix: modernize ui design

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,48 @@
-
 "use client";
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import Button from '@mui/material/Button';
+
 
 export default function Home() {
   const router = useRouter();
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-[#000D2E]">
-      <main className="flex flex-1 w-full mr-auto flex-row items-end py-32 px-16 bg-white dark:bg-[#000D2E]" style={{ alignItems: "flex-end" }} >
-      
-        <Image
-          
-          src="/claudinho.jpeg"
-          alt="claudinho.jpeg"
-          width={600}
-          height={600}
-          
-          priority
-        />
+   <div className="flex flex-col w-full min-h-screen font-inter">
+  <main className="grid grid-cols-1 md:grid-cols-2 w-full flex-1">
 
-          <div className="flex flex-col justify-between h-[500px]">
-    
-    <div>
-      <h1 className="text-4xl font-bold text-white"> Brainy! 
-        Seu assistente de IA para conquistar sua aprovação. </h1>
-      <p className="text-zinc-400">Pratique redação, revise conteúdos e chegue no dia do vestibular preparado.</p>
+    <div className="w-full h-full flex items-center justify-center bg-[#0b0438] py-16 md:py-32 px-6 md:px-16">
+      <Image
+        src="/brain.png"
+        alt="brain.png"
+        width={800}
+        height={400}
+        className="w-40 h-40 md:w-[800px] md:h-[400px]"
+        priority
+      />
     </div>
-    
 
+    <div className="w-full h-full bg-[#4F4F4F] flex flex-col justify-center gap-6 py-16 md:py-32 px-6 md:px-16">
+      <div>
+        <h1 className="text-4xl font-sans text-white">
+          Chegue no dia do vestibular preparado com o Brainy Assistant
+        </h1>
+        <p className="text-white dark:text-zinc-400 mt-4">
+   Pratique redação, revise conteúdos com aulas e exercícios, e acompanhe seu progresso em um só lugar.
+        </p>
+      </div>
 
-
-       <div className="flex flex-col gap-4">
-          <a className="flex h-12 items-center justify-center gap-2 rounded-full bg-foreground px-8 text-background hover:bg-[#383838] dark:hover:bg-[#ccc]"
-            href="/redacao" onClick={() => router.push("/redacao")}>
-            Redação
-         
-          </a>
-          <a className="flex h-12 items-center justify-center rounded-full border border-white/20 px-8 text-white hover:bg-white/10"
-            href="/mural" onClick={() => router.push("/mural")}>
-            Mural
-          </a>
-        </div>
-        </div>
-
-      </main>
-  
+      <div className="flex flex-row gap-4">
+        <Button variant="contained" size="medium" style={{ minWidth: 120, textTransform: "none" }} onClick={() => router.push("/redacao")}>
+          Redação
+        </Button>
+        <Button variant="outlined" size="medium" style={{ minWidth: 120, textTransform: "none" }} onClick={() => router.push("/mural")}>
+          Mural
+        </Button>
+      </div>
     </div>
+
+  </main>
+</div>
   );
 }
-
