a1337c11e38423de1d9f78d13b68d46a2fe98fd6
llGiovannall <laqi775@gmail.com>
2026-07-28 20:06:56 -0300

    feat: new mural page (under construction)

--- app/mural/page.tsx
+++ app/mural/page.tsx
@@ -1,25 +1,28 @@
 "use client";
 
-
-
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 export default function WelcomeCard(){
      const router = useRouter();
      return (
+
     <section className="relative overflow-hidden rounded-3xl bg-sidebar-gradient p-6 text-white">
+
+      <Tabs defaultValue="account" className="w-fit mb-6">
+        <TabsList className="flex flex-col h-auto w-fit gap-1">
+          <TabsTrigger value="account">Account</TabsTrigger>
+          <TabsTrigger value="password">Password</TabsTrigger>
+        </TabsList>
+      </Tabs>
+
       <div className="flex flex-col gap-3 md:flex-row md:items-center md:justify-between">
-        {/* Left Side (Text) */}
         <div className="max-w-xl md:w-3/5">
-          <h1 className="text-balance text-3xl font-semibold">Welcome back, Jennifer</h1>
-          <p className="mt-2 text-sm leading-6 text-white/90">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed diam nonummy nibh euismod tincidunt.
-          </p>
           <div onClick={()=> router.push("/banco_de_questoes")}className="mt-2 flex w-64 h-32 items-center gap-4 rounded-2xl bg-white/10 px-3 py-5 text-2x1"  >
             <div>
               <div className="font-large" >Banco de questões</div>
-             
+
             </div>
           </div>
         </div>
@@ -42,5 +45,3 @@ export default function WelcomeCard(){
     </section>
   )
 }
-
-
